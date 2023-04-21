8f75985096f3a42ea04f3b67a7aba23ba38f4e10
camposds1 <camposds@yahoo.com.br>
2023-04-20 23:24:24 -0300

    Criação de model Paciente para inclusão no BD

--- pi1/pi1/models.py
+++ pi1/pi1/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+
+    nome = models.CharField(
+        max_length = 255,
+        null = False,
+        blank = False
+    )
+
+    sobrenome = models.CharField(
+        max_length = 255,
+        null = False,
+        blank = False
+    )
+
+    sexo = models.CharField(
+        max_length = 1,
+        null = False,
+        blank = False
+    )
+
+    data_nascimento = models.DateField(
+        null = False,
+        blank = False
+    )
+
+    cidade = models.CharField(
+        max_length = 50,
+        null = False,
+        blank = False
+    )
+
+    estado = models.CharField(
+        max_length = 2,
+        null = False,
+        blank = False
+    )
+
+    objetos = models.Manager()
